bf286117ca14e196359e96d8f2a52ee64b2acc16
ubergesundheit <ubergesundheit@googlemail.com>
2015-04-30 09:00:23 -0400

    revert convert script add postgis script

--- dl-and-convert.sh
+++ dl-and-convert.sh
@@ -17,15 +17,14 @@ unzip -q $ZIP_FILENAME -d tmp
 
 # convert it to geojson, process and minify json
 export SHAPE_ENCODING="" # no recoding, http://www.mail-archive.com/gdal-dev@lists.osgeo.org/msg12322.html
-ogr2ogr -skipfailures -update -append -nlt POLYGON -t_srs crs:84 -f "PostGreSQL" PG:"host=postgis user=postgres dbname=postgres password=aufgrabungen123" tmp/aufbrueche.shp
-#ogr2ogr -nlt POLYGON -lco COORDINATE_PRECISION=6 -f GeoJSON -t_srs crs:84 /vsistdout/ tmp/aufbrueche.shp | jq -r -f jq-filters | sed "s/@@date@@/`date --iso-8601=sec`/" > $OUTPUT_DIR/$TODAY.json
+ogr2ogr -nlt POLYGON -lco COORDINATE_PRECISION=6 -f GeoJSON -t_srs crs:84 /vsistdout/ tmp/aufbrueche.shp | jq -r -f jq-filters | sed "s/@@date@@/`date --iso-8601=sec`/" > $OUTPUT_DIR/$TODAY.json
 
 # gzip it..
-#gzip -1 -f -c $OUTPUT_DIR/$TODAY.json > $OUTPUT_DIR/$TODAY.json.gz;
+gzip -1 -f -c $OUTPUT_DIR/$TODAY.json > $OUTPUT_DIR/$TODAY.json.gz;
 
 # copy to ../latest.json
-#ln -f -s $OUTPUT_DIR/$TODAY.json $OUTPUT_DIR/../latest.json
-#ln -f -s $OUTPUT_DIR/$TODAY.json.gz $OUTPUT_DIR/../latest.json.gz
+ln -f -s $OUTPUT_DIR/$TODAY.json $OUTPUT_DIR/../latest.json
+ln -f -s $OUTPUT_DIR/$TODAY.json.gz $OUTPUT_DIR/../latest.json.gz
 
 # cleanup
 rm -rf tmp $ZIP_FILENAME
